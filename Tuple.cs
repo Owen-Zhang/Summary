f279749d52ff8022a94e2494f1a5725550851be2
Owen-Zhang <1034992601@qq.com>
2016-04-09 16:51:04 +0800

    Create Tuple.cs

---
+++ Tuple.cs
@@ -0,0 +1,7 @@
+public Tuple<int, List<RefundReport>> XXXX()
+{
+  var totalCount = 100;
+  var list = new List<RefundReport>();
+  retrun 
+    new Tuple<int, List<RefundReport>>(totalCount, list);
+}
