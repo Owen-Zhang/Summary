e5fe2356497e8d1b20d32d567d66d77da2413ad2
Owen-Zhang <1034992601@qq.com>
2016-07-22 09:47:38 +0800

    Create Request-Url参数解析.cs

---
+++ Request-Url参数解析.cs
@@ -0,0 +1,27 @@
+网址：http://localhost:1897/News/Press/Content.aspx/123?id=1#toc
+
+Request.ApplicationPath	/
+Request.PhysicalPath	D:\Projects\Solution\web\News\Press\Content.aspx
+System.IO.Path.GetDirectoryName(Request.PhysicalPath)	D:\Projects\Solution\web\News\Press
+Request.PhysicalApplicationPath	D:\Projects\Solution\web\
+System.IO.Path.GetFileName(Request.PhysicalPath)	Content.aspx
+Request.CurrentExecutionFilePath	/News/Press/Content.aspx
+Request.FilePath	/News/Press/Content.aspx
+Request.Path	/News/Press/Content.aspx/123
+Request.RawUrl	/News/Press/Content.aspx/123?id=1
+Request.Url.AbsolutePath	/News/Press/Content.aspx/123
+Request.Url.AbsoluteUri	http://localhost:1897/News/Press/Content.aspx/123?id=1
+Request.Url.Scheme	http
+Request.Url.Host	localhost
+Request.Url.Port	1897
+Request.Url.Authority	localhost:1897
+Request.Url.LocalPath	/News/Press/Content.aspx/123
+Request.PathInfo	/123
+Request.Url.PathAndQuery	/News/Press/Content.aspx/123?id=1
+Request.Url.Query	?id=1
+Request.Url.Fragment	 
+Request.Url.Segments	/ 
+News/ 
+Press/ 
+Content.aspx/ 
+123
