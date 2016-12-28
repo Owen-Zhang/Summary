78b2e168866b271a1aa7aac91ea8f7f0a9816f2a
Owen-Zhang <1034992601@qq.com>
2016-12-28 22:22:06 +0800

    Create StudyBat.bat

---
+++ StudyBat.bat
@@ -0,0 +1,18 @@
+@echo off 
+
+:: set str= c d e f g h i j k l m n o p q r s t u v w x y z 
+
+:: echo  当前硬盘的分区有： 
+
+:: for %%i in (%str%) do if exist %%i: echo %%i;
+
+
+::输出当前目录 pushd调整当前目录，popd 返回当前目录 for 输出当目录中的所有*.cs文件 ^
+::echo %cd%
+::pushd D:\code\ConsoleTest\src\Model
+::for  %%i in (*.cs) do echo "%%i"
+::popd 
+::echo %cd%
+
+
+pause 
