c6b4005c636f981c7fcce4ef156ae3c97b8cfbcc
Owen-Zhang <1034992601@qq.com>
2017-03-29 15:39:29 +0800

    Delete 黑白名单(或者当有数据时就要在里面，当没有时就过).sql

--- C#/黑白名单(或者当有数据时就要在里面，当没有时就过).sql
+++
@@ -1,2 +0,0 @@
-(NOT EXISTS ( SELECT TOP (1) 1 FROM @tmpSpecialList ) OR temp.sellerid IN ( SELECT SellerID FROM @tmpSpecialList))
-      AND temp.sellerid NOT IN (SELECT  SellerID FROM @tmpBlockList)
