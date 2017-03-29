6178bcec67532bb3f3121d48d9da612e852e17d4
Owen-Zhang <1034992601@qq.com>
2017-03-29 16:35:19 +0800

    Update 黑白名单(或者当有数据时就要在里面，当没有时就过).sql

--- SQL/黑白名单(或者当有数据时就要在里面，当没有时就过).sql
+++ SQL/黑白名单(或者当有数据时就要在里面，当没有时就过).sql
@@ -1,2 +1,8 @@
 (NOT EXISTS ( SELECT TOP (1) 1 FROM @tmpSpecialList ) OR temp.sellerid IN ( SELECT SellerID FROM @tmpSpecialList))
       AND temp.sellerid NOT IN (SELECT  SellerID FROM @tmpBlockList)
+      
+      
+      
+INSERT  INTO @tmpSpecialList(SellerID)
+SELECT  T.c.value('(./text())[1]', 'Char(6)')
+FROM    @XMLSpecialList.nodes('/SpecialSellerList/SpecialSeller') AS T ( c )
