c698d814d993fcdbe759f24a059c7f2c636ee8a3
Owen-Zhang <1034992601@qq.com>
2017-03-30 10:31:21 +0800

    Create C#4.0调用htttps的设置.cs

---
+++ C#/C#4.0调用htttps的设置.cs
@@ -0,0 +1,4 @@
+ServicePointManager.SecurityProtocol = (SecurityProtocolType)48 | (SecurityProtocolType)192
+                | (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+                
+//ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
