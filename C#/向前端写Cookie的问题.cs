21dfc7d9e7014a34d0b341b542629b9629efa153
Owen-Zhang <1034992601@qq.com>
2017-08-17 19:13:42 +0800

    Create 向前端写Cookie的问题.cs

---
+++ C#/向前端写Cookie的问题.cs
@@ -0,0 +1,19 @@
+//去掉末尾多余的0
+decimal g = 10.01M;
+Console.WriteLine(g.ToString("#0.##"));
+
+StringBuilder sb = new StringBuilder();
+sb.AppendFormat("{0}={1}", cookieName, cookieValue);
+sb.AppendFormat(";domain={0}", parameters["domain"]);
+sb.AppendFormat(";path={0}", parameters["path"]);
+//配置为0，则cookie在会话结束后失效
+if (expires > 0)
+{
+    // 用max-age取代expires 使用相对时间来作为有效期
+    sb.AppendFormat(";max-age={0}", Convert.ToString(expires * 60));
+}
+
+HttpContext.Current.Response.AddHeader("Set-Cookie", sb.ToString());
+
+
+//也可以用HttpContext.Current.Response.cookies.add(new cookie());
