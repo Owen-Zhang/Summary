aaa9c6ea782d7c2e3b0a1da7d021b7a91e2463e3
Owen-Zhang <1034992601@qq.com>
2017-08-23 11:02:19 +0800

    Create IE8不支持滤镜.js

---
+++ UI/IE8不支持滤镜.js
@@ -0,0 +1,3 @@
+$('body').append("<div id=\"overlay\" style=\"position: fixed; top: 0px; left: 0px; right: 0px; bottom: 0px; background-color: rgb(0, 0, 0); background-image: none; background-repeat: repeat; background-position: 0% 0%; z-index: 999; opacity: 0.1; filter: progid:DXImageTransform.Microsoft.Alpha(opacity=10); display: block;\"></div>")
+
+注意这段代码：filter: progid:DXImageTransform.Microsoft.Alpha(opacity=10);
