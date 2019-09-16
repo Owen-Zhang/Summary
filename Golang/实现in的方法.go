89fc5be41039ec2966bd80a69550f5cd8f8d8864
Owen-Zhang <1034992601@qq.com>
2019-09-16 09:23:06 +0800

    Create 实现in的方法.go

---
+++ Golang/实现in的方法.go
@@ -0,0 +1,15 @@
+func In(haystack interface{}, needle interface{}) (bool, error) {
+    sVal := reflect.ValueOf(haystack)
+    kind := sVal.Kind()
+    if kind == reflect.Slice || kind == reflect.Array {
+        for i := 0; i < sVal.Len(); i++ {
+            if sVal.Index(i).Interface() == needle {
+                return true, nil
+            }
+        }
+
+        return false, nil
+    }
+
+    return false, ErrUnSupportHaystack
+}
