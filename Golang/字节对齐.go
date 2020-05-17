598a21c7e54ba21fd8a329dcf4a08c32d6c55f2c
Owen-Zhang <1034992601@qq.com>
2020-05-17 17:34:51 +0800

    Update 字节对齐.go

--- Golang/字节对齐.go
+++ Golang/字节对齐.go
@@ -5,3 +5,7 @@ uint32, int32, float32                4
 uint64, int64, float64, complex64     8
 complex128                            16
 struct{}, [0]T{}                      0
+
+arrary                                由其原素(element)的类型决定
+struct                                由其字段(field)的类型决定
+other types                           一个机器字的大小
