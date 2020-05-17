3de01211e12abb5248d0851cf0a95e18c3e5806d
Owen-Zhang <1034992601@qq.com>
2020-05-17 17:28:27 +0800

    Create 字节对齐.go

---
+++ Golang/字节对齐.go
@@ -0,0 +1,7 @@
+type                                  size in bytes
+byte, uint8, int8                     1
+uint16, int16                         2
+uint32, int32, float32                4
+uint64, int64, float64, complex64     8
+complex128                            16
+struct{}, [0]T{}                      0
